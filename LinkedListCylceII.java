73cdf55a03c1083c18bf70f9f83a581c9220df04
Mine Belgaid <amin-belgaid@hotmail.com>
2021-07-23 18:39:15 +0100

    LinkedListCyle II Solution

---
+++ LinkedListCylceII.java
@@ -0,0 +1,33 @@
+public class LinkedListCylceII {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            if (head == null)
+                return null;
+            ListNode slow = head, fast = head;
+            while (fast.next != slow) {
+                if (fast.next == null || fast.next.next == null)
+                    return null;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            slow = slow.next;
+            ListNode cur = head;
+            while (cur != slow) {
+                cur = cur.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+}
